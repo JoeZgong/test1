5c5b754b440a46c89f1b71b339ab9c60cf544e68
JoeZgong <43588835+JoeZgong@users.noreply.github.com>
2018-09-25 15:10:44 -0700

    sdss
    dsd

---
+++ pb_test.cpp
@@ -0,0 +1,34 @@
+// pb_test.cpp : Defines the entry point for the console application.
+//
+
+
+#include <stdio.h>
+#include <Windows.h>
+
+#include "ProgressBar.h"
+
+
+
+int main()
+{
+	ProgressBar *gProgressBar;
+
+	gProgressBar = new ProgressBar(std::string("Hello, ProgressBar Tesing!!"));
+
+	int pos = 0;
+
+	for (int index = 0; index <= 10; index++)
+	{
+		Sleep(1000);
+		gProgressBar->setPercentageDone(pos);
+		pos += 10;
+	}
+
+
+	printf("\nPress Any Key To Quit ... ...\n");
+	getchar();
+
+	delete gProgressBar;
+    return 0;
+}
+
